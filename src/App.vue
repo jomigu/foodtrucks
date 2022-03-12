9521e3a26cd24069a77716f3be26872fc1556fb9
Romeo <rommar@jpj.com>
2022-03-12 19:02:42 +0800

    Simple Google Authentication

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <template>
   <nav>
+    <p v-if="email">
+      <small>Logged-in as {{ email }}</small>
+    </p>
+    <p v-else>
+      <button @click="signIn" type="button" class="btn btn-primary">Google Log-in</button>
+    </p>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import { inject } from 'vue'
+
+export default {
+  data() {
+    return {
+      email: null,
+      authCode: null,
+    }
+  },
+  methods: {
+    async signIn() {
+      this.email = await this?.$gAuth?.instance?.currentUser?.get()?.getBasicProfile()?.getEmail()
+      if (this.email) {
+        this.authCode = this.$gAuth.getAuthCode()
+      } else {
+        this?.$gAuth?.signIn()
+      }
+    },
+    async signOut() {
+      await this.$gAuth.signOut()
+      await this.$gAuth.signIn()
+    },
+    authenticated() {
+      return this.authCode != null
+    }
+  },
+  setup() {
+    const Vue3GoogleOauth = inject("Vue3GoogleOauth")
+    return {
+      Vue3GoogleOauth,
+    };
+  },
+}
+</script>
 
 <style>
 #app {
