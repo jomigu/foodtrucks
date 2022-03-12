a46c4d3b2049f87265fe0050cf6db50480668817
Romeo <rommar@jpj.com>
2022-03-12 19:59:15 +0800

    Updated deploy script - separating local set-up from prod

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 
 const app = createApp(App)
 
+const clientId = process.env.CLIENT_ID || '18495362772-rftckb2iktu6689uurl8nbi06vr2cq7s.apps.googleusercontent.com'
+
 app.use(router).mount('#app')
 app.use(
     gapi,
     {
-        clientId: "18495362772-rftckb2iktu6689uurl8nbi06vr2cq7s.apps.googleusercontent.com",
+        clientId,
         scope: 'email',
         prompt: 'consent',
         fetch_basic_profile: false
