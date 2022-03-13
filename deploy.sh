cb20dbbdd4420105ae1a81e57b65f641413d36a4
Romeo <rommar@jpj.com>
2022-03-13 22:52:52 +0800

    Added scripts for dev and deployment

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycbx7dSXvmv_lNIualRBwYMBRL1axzE_cFpVyc_QER0h2Q1n0BrkntSTd2_WoJBviY-0g/exec
+export CLIENT_ID=18495362772-vob6o2nbnbd33cgrqat9gfmm8nl00hn6.apps.googleusercontent.com
+
+npm run deploy
---
+++ dev.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+export DEV_PORT=4020
+
+npm run serve
