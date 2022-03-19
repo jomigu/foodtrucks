7607eae1f9a58f9aa01bb6603c5995b4064fc187
Romeo <rommar@jpj.com>
2022-03-19 18:23:33 +0800

    Updated CLIENT_ID in prod

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycbzxjefihPyou5AdqrIHEJf3RnVNEzayuV8lzuKt3ua3xODSs0t3tJnEXwEiIZAw-hrK/exec
-export CLIENT_ID=18495362772-vob6o2nbnbd33cgrqat9gfmm8nl00hn6.apps.googleusercontent.com
+export CLIENT_ID=18495362772-nbpqq763ct061mfc07vtaaojkt0h2eir.apps.googleusercontent.com
 
 npm run deploy
