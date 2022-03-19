b534cad6537f084f5792b338a176a15b99771570
Romeo <rommar@jpj.com>
2022-03-19 20:01:22 +0800

    Updated submit order - set current date always

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycbzxjefihPyou5AdqrIHEJf3RnVNEzayuV8lzuKt3ua3xODSs0t3tJnEXwEiIZAw-hrK/exec
+export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycby98lwk4hb8SN1Mls8il0W6ksDN0xkJYxWX7b1Dw_ehFJY0F-QA-1jijGvOtvc2zSRV/exec
 export CLIENT_ID=18495362772-nbpqq763ct061mfc07vtaaojkt0h2eir.apps.googleusercontent.com
 
 npm run deploy
--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycbzxjefihPyou5AdqrIHEJf3RnVNEzayuV8lzuKt3ua3xODSs0t3tJnEXwEiIZAw-hrK/exec
+export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycby98lwk4hb8SN1Mls8il0W6ksDN0xkJYxWX7b1Dw_ehFJY0F-QA-1jijGvOtvc2zSRV/exec
 export CLIENT_ID=18495362772-rftckb2iktu6689uurl8nbi06vr2cq7s.apps.googleusercontent.com
 export DEV_PORT=4020
 
